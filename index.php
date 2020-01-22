fdd29beedabb2d4e5dbdf4fc73bafbc70e07ed67
ericaokinawa <52237965+ericaokinawa@users.noreply.github.com>
2020-01-23 08:18:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,35 @@
 
 // 1. 一番最初にindex.phpにアクセスしたら、入力画面を出す
 // 2. 入力データを入力後送信ボタンが押されたら、確認画面を出す
-// 3. 入力データが空で送信ボタンが押されたら、未入力エラーを出す
-// 4. 確認画面で完了が押されたら、確認画面のデータを引き継いで入力画面に表示する
+// 3. 入力データが空で送信ボタンが押されたら、未入力エラーを出す、
+// 入力されてたら、入力データを表示する
+// 4. どのボタンが押されたか判別
+// 5. 確認画面で完了が押されたら、入力画面に表示する
+// 6. 入力画面に確認画面で入力されたデータを表示する
 //（完了しましたという文字列も表示する）
-// 5. 確認画面で戻るをしたら、データを引き継いで入力画面を表示する
+// 7. 確認画面で戻るをしたら、データを引き継いで入力画面を表示する
 // （完了しましたという文字列は表示しない）
  
 // 変数の初期化
 $user_name = "";
 $user_address = "";
+$notes = "";
+
 $error_message = "";
+
 $input_completed_user_name ="";
+$input_completed_user_address ="";
+$input_completed_notes ="";
 
-
+// 1. 一番最初にindex.phpにアクセスしたら、入力画面を出す
 if (false) {
-    include('./inputform.php');
+    include('./inputform.html');
 } else {
-    include('./data.php');
+    include('./data.html');
 }
 
-// 入力チェック
+// 3. 入力データが空で送信ボタンが押されたら、未入力エラーを出す。
+// 入力されてたら、入力データを表示する
 function user_name() {
     $error_message = "<p style='color:red;'>*未入力です。</p>";
     $user_name = htmlspecialchars($_POST["user_name"],ENT_QUOTES, 'UTF-8');
@@ -42,43 +51,50 @@ function user_address() {
     }
 }
 
-
-// 入力確認画面
-function input_completed_user_name() {
-    $input_completed_user_name = htmlspecialchars($_POST["input_completed_user_name"],ENT_QUOTES, 'UTF-8');
-    if ($input_completed_user_name){
-        echo $user_name;
+function user_notes() {
+    $error_message = "<p style='color:red;'>*未入力です。</p>";
+    $user_notes = htmlspecialchars($_POST["user_notes"],ENT_QUOTES, 'UTF-8');
+    if (empty($user_notes)) {
+        echo $error_message;
     } else {
-
+        echo $user_notes;
     }
 }
 
-// function input_completed_user_address() {
-//     $input_completed_user_address = htmlspecialchars($_POST["input_completed_user_address"],ENT_QUOTES, 'UTF-8');
-//     if ($input_completed_user_address){
-//         echo $user_address;
-//     } else {
-
-//     }
-// }
-
-
-// ボタン判別確認
+// 4. どのボタンが押されたか判別
 function submit_user_name() {
     if ($_POST['submit_user_name']){
-        echo '名前の送信ボタンが押されました！';
+        echo '【名前（フリガナ）】の送信ボタンが押されました';
     } else {
 
     }
 }
 
-// function submit_user_address() {
-//     if ($_POST['submit_user_address']){
-//         echo '名前の送信ボタンが押されました！';
-//     } else {
+function submit_user_address() {
+    if ($_POST['submit_user_address']){
+        echo '【住所】の送信ボタンが押されました';
+    } else {
 
-//     }
-// }
+    }
+}
+
+function submit_user_notes() {
+    if ($_POST['submit_user_notes']){
+        echo '【備考】の送信ボタンが押されました';
+    } else {
+
+    }
+}
+
+// 6. 入力画面に確認画面で入力されたデータを表示する
+//（完了しましたという文字列も表示する）
+if ($compleets_data) {
+    echo "<a href='./inputform.html'></a>";
+} else {
+
+}
+
+
 
 
 
@@ -176,3 +192,5 @@ $data_out = file_put_contents('../../test/form.txt',$name.$address.$other);
 */
 
 ?>
+
+
